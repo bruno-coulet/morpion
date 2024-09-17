f7fafd1b427ba21afc24a71cb23c6ba168b6056b
bruno-coulet-data <bruno.coulet.pro@gmail.com>
2024-09-17 10:12:12 +0200

    premier jet

--- grid.py
+++ grid.py
@@ -1,6 +1,33 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Grille de jeu
 grid = np.zeros((3,3))
-
 print(grid)
+
+# Utiliser Pyplot pour dessiner les lignes de la grille.
+def draw_grid():
+    pass
+
+# fonction condition de victoire
+def victory():
+    pass
+# meme symbole sur toute une ligne
+# meme symbole sur toute une colonne
+# meme symbole sur  une diagonale
+
+# Dessine une croix
+def draw_cross():
+    if case=0 alors tu peux mettre une croix
+    pass
+
+# dessine un rond
+def draw_circle():
+    pass
+
+# alterner les joueurs
+while not victoire and 0 in grid:
+    joueur 1
+else:
+    joueur 2
+
